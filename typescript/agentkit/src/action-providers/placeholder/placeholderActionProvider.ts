9e9bc438dfede5835f5c42caefc7b3a695629ae0
George Mathew <97821867+0xgeorgemathew@users.noreply.github.com>
2025-02-09 11:38:43 +0530

    Stuff - Still works

--- typescript/agentkit/src/action-providers/placeholder/placeholderActionProvider.ts
+++ typescript/agentkit/src/action-providers/placeholder/placeholderActionProvider.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable multiline-comment-style */
 import { z } from "zod";
 import { ActionProvider } from "../actionProvider";
--- typescript/examples/langchain-cdp-chatbot/chatbot.ts
+++ typescript/examples/langchain-cdp-chatbot/chatbot.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable jsdoc/require-jsdoc */
 import { z } from "zod";
 
 import {
