41a86a99205daad1cbfbd57b01f6ff87710943b6
Philippe d'Argent <p.dargent@cern.ch>
2025-03-26 20:53:59 +0100

    added tests

--- typescript/agentkit/src/action-providers/truemarkets/utils.ts
+++ typescript/agentkit/src/action-providers/truemarkets/utils.ts
@@ -1,7 +1,10 @@
 import { MarketStatus } from "./constants";
 
 // Create mapping for status lookup
-export const GetMarketStatus = Object.entries(MarketStatus).reduce((acc, [key, value]) => {
+export const GetMarketStatus = Object.entries(MarketStatus).reduce(
+  (acc, [key, value]) => {
     acc[value] = key;
     return acc;
-  }, {} as Record<number, string>);
+  },
+  {} as Record<number, string>,
+);
